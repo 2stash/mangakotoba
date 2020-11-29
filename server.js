6642794b8731edb28c9329fa8ece341455bed1d2
Daniel Westlund <dwestlund531@gmail.com>
2020-11-28 20:02:33 -0600

    removed space in client/ build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/dataentry', require('./routes/api/dataentry'))
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/ build'));
+  app.use(express.static('client/build'));
   // app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
